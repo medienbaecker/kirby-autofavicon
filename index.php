d6a9e5f5854ddc3b9a1434c1848db1f9c3d0f4d4
Thomas Günther <mail@medienbaecker.com>
2020-08-14 11:07:21 +0200

    Prettify code ✨

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 Kirby::plugin('medienbaecker/autofavicon', [
 	'options' => [
 		'color' => '#000000',
-		'color_dark' => '#ffffff'
+		'color_dark' => '#ffffff',
+		'text' => kirby()->site()->title()->value()[0]
 	],
 	'snippets' => [
 		'autofavicon' => __DIR__ . '/snippets/autofavicon.php'
@@ -27,7 +28,7 @@ Kirby::plugin('medienbaecker/autofavicon', [
 						</style>
 						<mask id="text">
 							<rect x="0" y="0" width="100" height="100" fill="white" />
-							<text font-weight="bold" x="50" y="50" fill="black" text-anchor="middle" dominant-baseline="central" font-family="sans-serif" font-size="60">' . option('medienbaecker.autofavicon.text', kirby()->site()->title()->value()[0]) . '</text>
+							<text font-weight="bold" x="50" y="50" fill="black" text-anchor="middle" dominant-baseline="central" font-family="sans-serif" font-size="60">' . option('medienbaecker.autofavicon.text') . '</text>
 						</mask>
 						<circle mask="url(#text)" cx="50" cy="50" r="50"/>
 					</svg>', 'image/svg+xml'
