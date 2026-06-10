3a0c527d5d2220e973780c865fd54cee8a86b683
Thomas Günther <mail@medienbaecker.com>
2026-06-10 21:45:55 +0200

    Fix favicon route returning an empty response

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ Kirby::plugin('medienbaecker/autofavicon', [
 				$textColor = option('medienbaecker.autofavicon.text_color');
 				$textColorDark = option('medienbaecker.autofavicon.text_color_dark');
 
-				$svg = `
+				$svg = <<<SVG
 				<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
 					<style>
 						.bg {
@@ -48,7 +48,7 @@ Kirby::plugin('medienbaecker/autofavicon', [
 					<circle class="bg" cx="50" cy="50" r="50"/>
 					<text class="text" x="50" y="55" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-weight="bold" font-size="60">{$text}</text>
 				</svg>
-				`;
+				SVG;
 
 				return new Response($svg, 'image/svg+xml');
 			}
